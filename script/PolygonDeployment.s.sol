a737b17eb6bf3261b7c86558121445be40ea3ed4
tommy4241 <tommychan4241@gmail.com>
2023-11-14 12:22:31 +0900

    parse owner address from private key

--- script/PolygonDeployment.s.sol
+++ script/PolygonDeployment.s.sol
@@ -19,9 +19,8 @@ import "./Addresses.sol";
 // POLYGON DEPLOYMENT
 contract ContractScript is Script {
     function run() external {
-        address OWNER = 0x6E714c42438EC860bD3a50cbe104d2dab50193b3;
-        // address OWNER = 0x1246E96b7BC94107aa10a08C3CE3aEcc8E19217B;
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address OWNER = vm.addr(deployerPrivateKey);
         vm.startBroadcast(deployerPrivateKey);
         // first deploy the config
         int128 protocolFee = 50_000;
@@ -43,81 +42,86 @@ contract ContractScript is Script {
         IOracle sgdOracle = IOracle(Polygon.CHAINLINK_SGD_USD);
         IOracle trybOracle = IOracle(Polygon.CHAINLINK_TRY_USD);
         IOracle ngncOracle = IOracle(Polygon.CHAINLINK_NGNC_USD);
-        CurveFactoryV3.CurveInfo memory usdcUsdceCurveInfo = CurveFactoryV3.CurveInfo(
-            "dfx-usdc-usdce-v3",
-            "dfx-usdc-usdce-v3",
-            Polygon.USDCe,
-            Polygon.USDC,
-            CurveParams.BASE_WEIGHT,
-            CurveParams.QUOTE_WEIGHT,
-            usdOracle,
-            usdOracle,
-            CurveParams.ALPHA,
-            CurveParams.BETA,
-            CurveParams.MAX,
-            Polygon.USDCe_EPSILON,
-            CurveParams.LAMBDA
-        );
-        CurveFactoryV3.CurveInfo memory cadcUsdcCurveInfo = CurveFactoryV3.CurveInfo(
-            "dfx-cadc-usdc-v3",
-            "dfx-cadc-usdc-v3",
-            Polygon.CADC,
-            Polygon.USDC,
-            CurveParams.BASE_WEIGHT,
-            CurveParams.QUOTE_WEIGHT,
-            cadOracle,
-            usdOracle,
-            CurveParams.ALPHA,
-            CurveParams.BETA,
-            CurveParams.MAX,
-            Polygon.CADC_EPSILON,
-            CurveParams.LAMBDA
-        );
-        CurveFactoryV3.CurveInfo memory xsgdUsdcCurveInfo = CurveFactoryV3.CurveInfo(
-            "dfx-xsgd-usdc-v3",
-            "dfx-xsgd-usdc-v3",
-            Polygon.XSGD,
-            Polygon.USDC,
-            CurveParams.BASE_WEIGHT,
-            CurveParams.QUOTE_WEIGHT,
-            sgdOracle,
-            usdOracle,
-            CurveParams.ALPHA,
-            CurveParams.BETA,
-            CurveParams.MAX,
-            Polygon.XSGD_EPSILON,
-            CurveParams.LAMBDA
-        );
-        CurveFactoryV3.CurveInfo memory trybUsdcCurveInfo = CurveFactoryV3.CurveInfo(
-            "dfx-tryb-usdc-v3",
-            "dfx-tryb-usdc-v3",
-            Polygon.TRYB,
-            Polygon.USDC,
-            CurveParams.BASE_WEIGHT,
-            CurveParams.QUOTE_WEIGHT,
-            trybOracle,
-            usdOracle,
-            CurveParams.ALPHA,
-            CurveParams.BETA,
-            CurveParams.MAX,
-            Polygon.TRYB_EPSILON,
-            CurveParams.LAMBDA
-        );
-        CurveFactoryV3.CurveInfo memory ngncUsdcCurveInfo = CurveFactoryV3.CurveInfo(
-            "dfx-ngnc-usdc-v3",
-            "dfx-ngnc-usdc-v3",
-            Polygon.NGNC,
-            Polygon.USDC,
-            CurveParams.BASE_WEIGHT,
-            CurveParams.QUOTE_WEIGHT,
-            ngncOracle,
-            usdOracle,
-            CurveParams.ALPHA,
-            CurveParams.BETA,
-            CurveParams.MAX,
-            Polygon.NGNC_EPSILON,
-            CurveParams.LAMBDA
-        );
+        CurveFactoryV3.CurveInfo memory usdcUsdceCurveInfo = CurveFactoryV3
+            .CurveInfo(
+                "dfx-usdc-usdce-v3",
+                "dfx-usdc-usdce-v3",
+                Polygon.USDCe,
+                Polygon.USDC,
+                CurveParams.BASE_WEIGHT,
+                CurveParams.QUOTE_WEIGHT,
+                usdOracle,
+                usdOracle,
+                CurveParams.ALPHA,
+                CurveParams.BETA,
+                CurveParams.MAX,
+                Polygon.USDCe_EPSILON,
+                CurveParams.LAMBDA
+            );
+        CurveFactoryV3.CurveInfo memory cadcUsdcCurveInfo = CurveFactoryV3
+            .CurveInfo(
+                "dfx-cadc-usdc-v3",
+                "dfx-cadc-usdc-v3",
+                Polygon.CADC,
+                Polygon.USDC,
+                CurveParams.BASE_WEIGHT,
+                CurveParams.QUOTE_WEIGHT,
+                cadOracle,
+                usdOracle,
+                CurveParams.ALPHA,
+                CurveParams.BETA,
+                CurveParams.MAX,
+                Polygon.CADC_EPSILON,
+                CurveParams.LAMBDA
+            );
+        CurveFactoryV3.CurveInfo memory xsgdUsdcCurveInfo = CurveFactoryV3
+            .CurveInfo(
+                "dfx-xsgd-usdc-v3",
+                "dfx-xsgd-usdc-v3",
+                Polygon.XSGD,
+                Polygon.USDC,
+                CurveParams.BASE_WEIGHT,
+                CurveParams.QUOTE_WEIGHT,
+                sgdOracle,
+                usdOracle,
+                CurveParams.ALPHA,
+                CurveParams.BETA,
+                CurveParams.MAX,
+                Polygon.XSGD_EPSILON,
+                CurveParams.LAMBDA
+            );
+        CurveFactoryV3.CurveInfo memory trybUsdcCurveInfo = CurveFactoryV3
+            .CurveInfo(
+                "dfx-tryb-usdc-v3",
+                "dfx-tryb-usdc-v3",
+                Polygon.TRYB,
+                Polygon.USDC,
+                CurveParams.BASE_WEIGHT,
+                CurveParams.QUOTE_WEIGHT,
+                trybOracle,
+                usdOracle,
+                CurveParams.ALPHA,
+                CurveParams.BETA,
+                CurveParams.MAX,
+                Polygon.TRYB_EPSILON,
+                CurveParams.LAMBDA
+            );
+        CurveFactoryV3.CurveInfo memory ngncUsdcCurveInfo = CurveFactoryV3
+            .CurveInfo(
+                "dfx-ngnc-usdc-v3",
+                "dfx-ngnc-usdc-v3",
+                Polygon.NGNC,
+                Polygon.USDC,
+                CurveParams.BASE_WEIGHT,
+                CurveParams.QUOTE_WEIGHT,
+                ngncOracle,
+                usdOracle,
+                CurveParams.ALPHA,
+                CurveParams.BETA,
+                CurveParams.MAX,
+                Polygon.NGNC_EPSILON,
+                CurveParams.LAMBDA
+            );
         // Deploy all new Curves
         deployedCurveFactory.newCurve(usdcUsdceCurveInfo);
         deployedCurveFactory.newCurve(cadcUsdcCurveInfo);
