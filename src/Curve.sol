f266134bcb7f3d0206499e8d64ce3efa61d9798d
kafitz <kyle@dfx.finance>
2024-12-01 16:10:33 -0500

    Move Curves library to own file

--- src/Curve.sol
+++ src/Curve.sol
@@ -11,188 +11,27 @@
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.;
+pragma solidity ^0.8.27;
 
-pragma solidity ^0.8.13;
-pragma experimental ABIEncoderV2;
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-import "./interfaces/IWeth.sol";
-import "./interfaces/IAssimilatorFactory.sol";
-import "./lib/ABDKMath64x64.sol";
-import "./lib/FullMath.sol";
-import "./lib/NoDelegateCall.sol";
-import "./Orchestrator.sol";
-import "./ProportionalLiquidity.sol";
-import "./Swaps.sol";
-import "./ViewLiquidity.sol";
-import "./Storage.sol";
-import "./interfaces/ICurveFactory.sol";
-import "./interfaces/IAssimilator.sol";
-import "./interfaces/ICurve.sol";
-import "./interfaces/IConfig.sol";
-import "./Structs.sol";
-
-library Curves {
-    using ABDKMath64x64 for int128;
-
-    event Approval(address indexed _owner, address indexed spender, uint256 value);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    function add(uint256 x, uint256 y, string memory errorMessage) private pure returns (uint256 z) {
-        require((z = x + y) >= x, errorMessage);
-    }
-
-    function sub(uint256 x, uint256 y, string memory errorMessage) private pure returns (uint256 z) {
-        require((z = x - y) <= x, errorMessage);
-    }
-
-    /**
-     * @dev See {IERC20-transfer}.
-     *
-     * Requirements:
-     *
-     * - `recipient` cannot be the zero address.
-     * - the caller must have a balance of at least `amount`.
-     */
-    function transfer(Storage.Curve storage curve, address recipient, uint256 amount) external returns (bool) {
-        _transfer(curve, msg.sender, recipient, amount);
-        return true;
-    }
-
-    /**
-     * @dev See {IERC20-approve}.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     */
-    function approve(Storage.Curve storage curve, address spender, uint256 amount) external returns (bool) {
-        _approve(curve, msg.sender, spender, amount);
-        return true;
-    }
-
-    /**
-     * @dev See {IERC20-transferFrom}.
-     *
-     * Emits an {Approval} event indicating the updated allowance. This is not
-     * required by the EIP. See the note at the beginning of {ERC20};
-     *
-     * Requirements:
-     * - `sender` and `recipient` cannot be the zero address.
-     * - `sender` must have a balance of at least `amount`.
-     * - the caller must have allowance for `sender`'s tokens of at least
-     * `amount`
-     */
-    function transferFrom(Storage.Curve storage curve, address sender, address recipient, uint256 amount)
-        external
-        returns (bool)
-    {
-        _transfer(curve, sender, recipient, amount);
-        _approve(
-            curve, sender, msg.sender, sub(curve.allowances[sender][msg.sender], amount, "Curve/insufficient-allowance")
-        );
-        return true;
-    }
-
-    /**
-     * @dev Atomically increases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     */
-    function increaseAllowance(Storage.Curve storage curve, address spender, uint256 addedValue)
-        external
-        returns (bool)
-    {
-        _approve(
-            curve,
-            msg.sender,
-            spender,
-            add(curve.allowances[msg.sender][spender], addedValue, "Curve/approval-overflow")
-        );
-        return true;
-    }
-
-    /**
-     * @dev Atomically decreases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     * - `spender` must have allowance for the caller of at least
-     * `subtractedValue`.
-     */
-    function decreaseAllowance(Storage.Curve storage curve, address spender, uint256 subtractedValue)
-        external
-        returns (bool)
-    {
-        _approve(
-            curve,
-            msg.sender,
-            spender,
-            sub(curve.allowances[msg.sender][spender], subtractedValue, "Curve/allowance-decrease-underflow")
-        );
-        return true;
-    }
-
-    /**
-     * @dev Moves tokens `amount` from `sender` to `recipient`.
-     *
-     * This is public function is equivalent to {transfer}, and can be used to
-     * e.g. implement automatic token fees, slashing mechanisms, etc.
-     *
-     * Emits a {Transfer} event.
-     *
-     * Requirements:
-     *
-     * - `sender` cannot be the zero address.
-     * - `recipient` cannot be the zero address.
-     * - `sender` must have a balance of at least `amount`.
-     */
-    function _transfer(Storage.Curve storage curve, address sender, address recipient, uint256 amount) private {
-        require(sender != address(0), "ERC20: transfer from the zero address");
-        require(recipient != address(0), "ERC20: transfer to the zero address");
-
-        curve.balances[sender] = sub(curve.balances[sender], amount, "Curve/insufficient-balance");
-        curve.balances[recipient] = add(curve.balances[recipient], amount, "Curve/transfer-overflow");
-        emit Transfer(sender, recipient, amount);
-    }
-
-    /**
-     * @dev Sets `amount` as the allowance of `spender` over the `_owner`s tokens.
-     *
-     * This is public function is equivalent to `approve`, and can be used to
-     * e.g. set automatic allowances for certain subsystems, etc.
-     *
-     * Emits an {Approval} event.
-     *
-     * Requirements:
-     *
-     * - `_owner` cannot be the zero address.
-     * - `spender` cannot be the zero address.
-     */
-    function _approve(Storage.Curve storage curve, address _owner, address spender, uint256 amount) private {
-        require(_owner != address(0), "ERC20: approve from the zero address");
-        require(spender != address(0), "ERC20: approve to the zero address");
-
-        curve.allowances[_owner][spender] = amount;
-        emit Approval(_owner, spender, amount);
-    }
-}
+import {IAssimilator} from "./interfaces/IAssimilator.sol";
+import {IConfig} from "./interfaces/IConfig.sol";
+import {ICurve} from "./interfaces/ICurve.sol";
+import {ICurveFactory} from "./interfaces/ICurveFactory.sol";
+import {IWETH} from "./interfaces/IWeth.sol";
+import {ABDKMath64x64} from "./lib/ABDKMath64x64.sol";
+import {NoDelegateCall} from "./lib/NoDelegateCall.sol";
+import {Curves} from "./Curves.sol";
+import {Orchestrator} from "./Orchestrator.sol";
+import {ProportionalLiquidity} from "./ProportionalLiquidity.sol";
+import {Swaps} from "./Swaps.sol";
+import {ViewLiquidity} from "./ViewLiquidity.sol";
+import {Storage} from "./Storage.sol";
+import {DepositData, OriginSwapData, TargetSwapData} from "./Structs.sol";
 
 contract Curve is Storage, NoDelegateCall, ICurve {
     using SafeMath for uint256;
@@ -635,7 +474,7 @@ contract Curve is Storage, NoDelegateCall, ICurve {
         return ProportionalLiquidity.proportionalWithdraw(curve, _curvesToBurn, false);
     }
 
-    /// @notice  withdrawas amount of curve tokens from the the pool equally from the numeraire assets of the pool with no slippage
+    /// @notice  Withdraws amount of curve tokens from the the pool equally from the numeraire assets of the pool with no slippage
     /// @param   _curvesToBurn the full amount you want to withdraw from the pool which will be withdrawn from evenly amongst the
     ///                        numeraire assets of the pool
     /// @return withdrawals_ the amonts of numeraire assets withdrawn from the pool
@@ -650,7 +489,7 @@ contract Curve is Storage, NoDelegateCall, ICurve {
         return ProportionalLiquidity.proportionalWithdraw(curve, _curvesToBurn, false);
     }
 
-    /// @notice  withdrawas amount of curve tokens from the the pool equally from the numeraire assets of the pool with no slippage, WETH is unwrapped to ETH
+    /// @notice  Withdraws amount of curve tokens from the the pool equally from the numeraire assets of the pool with no slippage, WETH is unwrapped to ETH
     /// @param   _curvesToBurn the full amount you want to withdraw from the pool which will be withdrawn from evenly amongst the
     ///                        numeraire assets of the pool
     /// @return withdrawals_ the amonts of numeraire assets withdrawn from the pool
